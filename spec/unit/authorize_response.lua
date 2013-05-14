b3e498b83c9c422c62938f6d56e3375210a34eb8
Raimon Grau <raimonster@gmail.com>
2013-05-14 21:35:28 +0200

    authorize response added

---
+++ spec/unit/authorize_response.lua
@@ -0,0 +1,17 @@
+setup(function()
+         Response = require("threescale.authorize_response")
+      end)
+
+teardown(function()
+            response = nil
+         end)
+
+describe("authorize_response inherits from response",
+				function()
+					 it("creates a different object",
+							function()
+								 r = Response.new()
+								 r.set_to_succeeded()
+								 -- assert.is_true(r.succeeded())
+-							end)
+				end)
--- src/threescale/authorize_response.lua
+++ src/threescale/authorize_response.lua
@@ -6,10 +6,10 @@ function AuthorizeResponse.bless (self)
    local redirect_url
    local usage_reports = {}
 
-   function plan return plan end
-   function app_key return app_key end
-   function redirect_url return redirect_url end
-   function usage_reports return usage_reports end
+   function plan() return plan end
+   function app_key() return app_key end
+   function redirect_url() return redirect_url end
+   function usage_reports() return usage_reports end
 
    function add_usage_report(opts)
       table.insert(usage_reports, UsageReport.new(opts))
@@ -21,3 +21,4 @@ end
 function AuthorizeResponse.new()
    return AuthorizeResponse.bless(Response.new())
 end
+return AuthorizeResponse
